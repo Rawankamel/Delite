737eb9bdbf1414da08ca09eeafe4f57954a74988
yassmin.qasm@gmail.com <janait&mamdouh!1997>
2017-11-26 02:12:20 +0200

    logo

--- app/src/main/java/com/fci/delite/Main.java
+++ app/src/main/java/com/fci/delite/Main.java
@@ -1,13 +1,30 @@
 package com.fci.delite;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Main extends AppCompatActivity {
 
+    /*4second*/
+    private static  int Splash_time_out=4000;
+    /**/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/* alt+enter to import handler*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run()
+            {
+                Intent homeIntent=new Intent(Main.this,home.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },Splash_time_out);
+        /**/
+
     }
 }
