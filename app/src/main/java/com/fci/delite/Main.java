0484ca50578d0e61521be5615bba44de48aebf59
yassmin.qasm@gmail.com <janait&mamdouh!1997>
2017-12-01 03:27:40 +0200

    logo

--- app/src/main/java/com/fci/delite/Main.java
+++ app/src/main/java/com/fci/delite/Main.java
@@ -7,14 +7,30 @@ import android.os.Bundle;
 
 public class Main extends AppCompatActivity {
 
-    /*4second*/
+    /*4second 1-----d*
     private static  int Splash_time_out=4000;
     /**/
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-/* alt+enter to import handler*/
+        setContentView(R.layout.activity_home);
+
+       /*Thread thread=new Thread(){
+            @Override
+            public void run(){
+                try{
+                    sleep(3000);
+                    Intent intent=new Intent(getApplicationContext(),home.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();*/
+        /* alt+enter to import handler 1--d*
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run()
--- app/src/main/java/com/fci/delite/home.java
+++ app/src/main/java/com/fci/delite/home.java
@@ -1,16 +1,24 @@
 package com.fci.delite;
 
+
 import android.content.Intent;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class home extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+/****/
 
     }
 }
