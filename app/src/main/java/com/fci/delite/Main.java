1d80dc20c3be326e55ad335f40f9dc3a146b1453
yassmin.qasm@gmail.com <janait&mamdouh!1997>
2017-12-01 19:16:54 +0200

    logo

--- app/src/main/java/com/fci/delite/Main.java
+++ app/src/main/java/com/fci/delite/Main.java
@@ -7,40 +7,14 @@ import android.os.Bundle;
 
 public class Main extends AppCompatActivity {
 
-    /*4second 1-----d*
-    private static  int Splash_time_out=4000;
-    /**/
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main);
 
-       /*Thread thread=new Thread(){
-            @Override
-            public void run(){
-                try{
-                    sleep(3000);
-                    Intent intent=new Intent(getApplicationContext(),home.class);
-                    startActivity(intent);
-                    finish();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        thread.start();*/
-        /* alt+enter to import handler 1--d*
-        new Handler().postDelayed(new Runnable(){
-            @Override
-            public void run()
-            {
-                Intent homeIntent=new Intent(Main.this,home.class);
-                startActivity(homeIntent);
-                finish();
-            }
-        },Splash_time_out);
-        /**/
+
 
     }
 }
--- app/src/main/java/com/fci/delite/home.java
+++ app/src/main/java/com/fci/delite/home.java
@@ -1,9 +1,13 @@
 package com.fci.delite;
 
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.net.wifi.hotspot2.pps.HomeSp;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -11,14 +15,23 @@ import android.widget.ImageView;
 public class home extends AppCompatActivity {
 
 
-
+    private static  int Splash_time_out=4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-/****/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run()
+            {
+                Intent homeIntent=new Intent(home.this,Main.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },Splash_time_out);
+
 
     }
 }
