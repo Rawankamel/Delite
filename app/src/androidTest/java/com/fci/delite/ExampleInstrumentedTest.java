3eee81380c241cd34e2333c6e95877798ce4f205
BananEmad <33826865+BananEmad@users.noreply.github.com>
2017-12-06 02:56:17 +0200

    6

--- app/src/androidTest/java/com/fci/delite/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/fci/delite/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.fci.delite", appContext.getPackageName());
+        assertEquals("Delite", appContext.getPackageName());
     }
 }
